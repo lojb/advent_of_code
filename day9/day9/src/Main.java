604f885c3bfbc284a2d150f8d59a4dfa0504ff51
lojb <balazsloj02@gmail.com>
2023-12-12 11:40:02 +0100

    Create project for day 9

--- day9/day9/src/Main.java
+++ day9/day9/src/Main.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String filePath = "D:\\advent_of_code\\day8\\day8\\src\\input.txt";
+        //String filePath = "D:\\advent_of_code\\day8\\day8\\src\\example.txt";
+        Path path = Paths.get(filePath);
+        List<String> lines = Files.readAllLines(path);
     }
 }
